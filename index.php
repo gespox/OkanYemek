6db130ac729c0aa09372cbb4b0959354d3f04cc5
Sait Okan <2015123114@cumhuriyet.edu.tr>
2018-01-22 00:12:10 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Mühendislik Projesi</title>
+<link href="main.css" rel="stylesheet" type="text/css" />
+<link href="SpryAssets/SpryMenuBarVertical.css" rel="stylesheet" type="text/css" />
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+</head>
+<body>
+
+<div class="main">
+
+<div class="header"> 
+<img src="banner.jpg" width="100%" height="100%" />
+</div>
+
+<div class="icerik">
+<div class="menu"></div>
+<div class="giris">
+<div class="vertical-menu">
+  <a href="ekle.php" class="active">Kitap Ekle</a>
+  <a href="guncelle.php">Kitap Güncelle</a>
+  <a href="sil.php">Kitap Kaldır</a>
+  <a href="listele.php">Kitap Listele</a>
+</div>
+
+
+
+
+
+</div>
+</div>
+
+<div class="footer"> <br /><br />Bu Site Duygu Karakavuk Tarafından Yapılmıştır... &copy;2018</div>
+
+
+
+
+</div>
+
+</body>
+</html>
